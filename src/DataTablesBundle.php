270f94b423d991d1921e277554bdc8281eadf5be
Artem Rodygin <artem.rodygin@gmail.com>
2015-10-30 17:21:10 +1300

    Empty bundle

---
+++ src/DataTablesBundle.php
@@ -0,0 +1,20 @@
+<?php
+
+//----------------------------------------------------------------------
+//
+//  Copyright (C) 2015 Artem Rodygin
+//
+//  This file is part of DataTables Symfony bundle.
+//
+//  You should have received a copy of the MIT License along with
+//  the bundle. If not, see <http://opensource.org/licenses/MIT>.
+//
+//----------------------------------------------------------------------
+
+namespace DataTables;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class DataTablesBundle extends Bundle
+{
+}
---
+++ src/DependencyInjection/DataTablesExtension.php
@@ -0,0 +1,36 @@
+<?php
+
+//----------------------------------------------------------------------
+//
+//  Copyright (C) 2015 Artem Rodygin
+//
+//  This file is part of DataTables Symfony bundle.
+//
+//  You should have received a copy of the MIT License along with
+//  the bundle. If not, see <http://opensource.org/licenses/MIT>.
+//
+//----------------------------------------------------------------------
+
+namespace DataTables\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+
+/**
+ * This is the class that loads and manages the bundle configuration.
+ *
+ * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
+ */
+class DataTablesExtension extends Extension
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+        $loader->load('datatables.yml');
+    }
+}
